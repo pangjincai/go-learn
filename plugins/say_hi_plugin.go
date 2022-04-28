676bad3f37dfd029fc2bcdcd94c423ece55aff12
pangjincai <pangjincai@tm.video>
2022-04-28 18:45:15 +0800

    fix

--- plugins/say_hi_plugin.go
+++ plugins/say_hi_plugin.go
@@ -1,14 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"log"
+)
 
 var Hello string //必须是大写，小写的时候提示找不着此 symbol
 
 //加载插件时会被调用
 func init()  {
-	fmt.Println("Test plugin init")
+	log.Println("Test plugin init")
 }
 
 func SayFunc()  {
-	fmt.Printf("Say func will print %s %s\n", Hello, "World！") //必须是大写，小写的时候提示找不着此 symbol
+	log.Printf("Say func will print %s %s\n", Hello, "World！") //必须是大写，小写的时候提示找不着此 symbol
 }
